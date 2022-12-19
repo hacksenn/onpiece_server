bad307f06f13859dcaaf4a52f2c6242b835b256b
kimmand0o0 <yuwang0130@gmail.com>
2022-12-19 23:44:28 +0900

    :art: 회원가입, 중복검사 api 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 router.use('/posts', require('./comment'))
 router.use('/users', require('./user'))
-router.use('/signup', require('./signup'))
 router.use('/posts', require('./post'));
 
 module.exports = router;
