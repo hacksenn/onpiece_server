6623a6982cbbd22f75ee065322d7f4ea0b64a7d9
kimmand0o0 <yuwang0130@gmail.com>
2022-12-22 01:17:38 +0900

    :memo: 프론트 요청으로 userId 전달 방식 수정

--- src/routes/user.js
+++ src/routes/user.js
@@ -29,21 +29,21 @@ router.get('/signup/emailNnickname', userController.checkUser);
 //==================================
 //        작성한 스터디 목록 조회
 //==================================
-router.get('/:userId/posts', authUser, userController.FindAllUserPosts);
+router.get('/posts', authUser, userController.FindAllUserPosts);
 
 //==================================
 //        신청한 스터디 목록 조회
 //==================================
-router.get('/:userId/apply', authUser, userController.FindAllUserApply);
+router.get('/apply', authUser, userController.FindAllUserApply);
 
 //==================================
 //           유저 정보 조회
 //==================================
-router.get('/:userId', authUser, userController.GetUser);
+router.get('/', authUser, userController.GetUser);
 
 //==================================
 //           유저 정보 수정
 //==================================
-router.put('/:userId', authUser, userController.UpdateUser);
+router.put('/', authUser, userController.UpdateUser);
 
 module.exports = router;
