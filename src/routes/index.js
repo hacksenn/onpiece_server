850ef07c06f5aa185de50cbc5fea70ff5c5584fc
kimmand0o0 <yuwang0130@gmail.com>
2022-12-22 01:38:49 +0900

    긴급수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const authUser = require('../middleWares/authUser.middleware');
 
 router.use('/users', require('./user'));
-router.use('/posts', require('./user'));
 router.use('/posts', authUser, require('./comment'));
 router.use('/posts', authUser, require('./post'));
+router.use('/posts', require('./user'));
 
 module.exports = router;
